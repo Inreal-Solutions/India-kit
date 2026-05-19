26c2d29e80f3a8ab3b7ae970bfe5dee3a031a812
CI <you@example.com>
2026-05-19 17:41:39 +0530

    chore: update project files

--- node_modules/.pnpm/node_modules/india-kit/tsup.config.ts
+++ node_modules/.pnpm/node_modules/india-kit/tsup.config.ts
@@ -1,12 +1,23 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-  entry: ['src/index.ts'],
-  dts: true,
-  format: ['cjs', 'esm'],
-  sourcemap: true,
-  clean: true,
-  noExternal: ['@india-kit/core'],
-  splitting: false,
-  outDir: 'dist',
-});
+export default defineConfig([
+  {
+    entry: ['src/index.ts'],
+    dts: true,
+    format: ['cjs', 'esm'],
+    sourcemap: true,
+    clean: true,
+    noExternal: ['@india-kit/core'],
+    splitting: false,
+    outDir: 'dist',
+  },
+  {
+    entry: ['src/bin.ts'],
+    format: ['cjs'],
+    sourcemap: false,
+    noExternal: ['@india-kit/core'],
+    splitting: false,
+    outDir: 'dist',
+    banner: { js: '#!/usr/bin/env node' },
+  },
+]);
